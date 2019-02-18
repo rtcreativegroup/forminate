97b6eabfd28da59557693dd9d4bd73ce705d004a
Brent Odell <brentdodell@gmail.com>
2019-02-18 11:54:29 -0600

    Allow filtered rspec runs (i.e. :focus)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,5 +16,12 @@ ActiveRecord::Schema.define do
   end
 end
 
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.filter_run_excluding broken: true
+  config.run_all_when_everything_filtered = true
+end
+
 # Requires supporting files in spec/support/
 Dir["#{File.dirname(__FILE__)}/support/*.rb"].each { |file| require file }
